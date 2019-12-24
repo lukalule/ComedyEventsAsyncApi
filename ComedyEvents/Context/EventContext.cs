625f75a78ee54b1c6c81d7311659cc75acb224bd
lukananic <lukananic2@gmail.com>
2019-12-24 21:32:02 +0100

    Added DbContext

---
+++ ComedyEvents/Context/EventContext.cs
@@ -0,0 +1,25 @@
+﻿using ComedyEvents.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComedyEvents.Context
+{
+    public class EventContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public EventContext(IConfiguration configuration, DbContextOptions options) : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Comedian> Comedians { get; set; }
+        public DbSet<Venue> Venues { get; set; }
+    }
+}
--- ComedyEvents/Startup.cs
+++ ComedyEvents/Startup.cs
@@ -15,7 +15,7 @@ namespace ComedyEvents
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,10 +26,12 @@ namespace ComedyEvents
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            app.UseMvc();
         }
     }
 }
