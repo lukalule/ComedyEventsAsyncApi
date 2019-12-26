61cb608bd3a2c3a48cdb8d240dbb489330bf1e60
lukananic <lukananic2@gmail.com>
2019-12-26 15:22:47 +0100

    add events controller ignore reference looping in services.addmvc

---
+++ ComedyEvents/Controllers/EventsController.cs
@@ -0,0 +1,37 @@
+﻿using ComedyEvents.Models;
+using ComedyEvents.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComedyEvents.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly IEventRepository _repository;
+
+        public EventsController(IEventRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Event[]>> Get(bool includeGigs = false)
+        {
+            try
+            {
+                var result =  await _repository.GetEvents(includeGigs);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+        }
+    }
+}
